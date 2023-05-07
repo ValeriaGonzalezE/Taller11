a324971f56c063272eac8a7b0e0de7787d37fd9d
unknown <valeriagoes170805@gmail.com>
2023-05-06 21:40:43 -0500

    primer commit

---
+++ TallerPolimorfismo/src/ActividadOnce/Main.java
@@ -0,0 +1,10 @@
+package ActividadOnce;
+
+public class Main {
+
+	 public static void main(String[] args) {
+		    Factorial factorial = new Factorial(10);
+		    System.out.println(factorial);
+		  }
+	
+}
